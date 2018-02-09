3e7e8f634dc9cff8663e250210c8c33c70fb6b2b
Braagaa <michael_braga@outlook.com>
2018-02-09 18:45:12 -0500

    finished up documentation to js/errors.js

--- js/errors.js
+++ js/errors.js
@@ -27,6 +27,15 @@ const checkError = R.curry(function(type, message, err) {
  */
 const requestError = checkError('RequestError');
 
+/**
+ * Gets the message property of an Error Object and logs it to 
+ * console.log and console.error. The console.log is needed to log it 
+ * to the console, while the console.error is needed in order to log it
+ * to the stderr so it can be properly be logged to a file.
+ *
+ * @param {Object} - Error Object
+ * @returns {string} The message property in a Error Object
+ */
 const logOutError = R.pipe(R.prop('message'), R.tap(console.log), R.tap(console.error));
 
 module.exports = {requestError, logOutError};
