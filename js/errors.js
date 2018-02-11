c2f5c0810ac0d706fac891126fbb63329e8f05a7
Braagaa <michael_braga@outlook.com>
2018-02-11 00:20:00 -0500

    Logging errors now log to the console and to a log file with a timestamp

--- js/errors.js
+++ js/errors.js
@@ -1,5 +1,7 @@
 const R = require('ramda');
 
+const {now} = require('./time');
+
 /**
  * If a err.name match the name we are look for, changes the 
  * message of the error.message to our liking and rethrows the Error Object.
@@ -28,6 +30,21 @@ const checkError = R.curry(function(type, message, err) {
 const requestError = checkError('RequestError');
 
 /**
+ * Creates a string of the current date and time. The date is inclosed in
+ * [].
+ *
+ * @example [Sat Feb 10 2018 22:44:25 GMT-0500]
+ * @param {Void}
+ * @returns {string} Formatted date 
+ */
+const formatedErrorDate = R.pipe(
+    now('ddd MMM DD YYYY HH:mm:ssZZ'),
+    R.prepend('['), 
+    R.append(']'),
+    R.join('')
+);
+
+/**
  * Gets the message property of an Error Object and logs it to 
  * console.log and console.error. The console.log is needed to log it 
  * to the console, while the console.error is needed in order to log it
@@ -36,6 +53,12 @@ const requestError = checkError('RequestError');
  * @param {Object} - Error Object
  * @returns {string} The message property in a Error Object
  */
-const logOutError = R.pipe(R.prop('message'), R.tap(console.log), R.tap(console.error));
+const logOutError = R.pipe(
+    R.prop('message'), 
+    R.tap(console.log), 
+    R.juxt([formatedErrorDate, R.identity]),
+    R.join(' '),
+    R.tap(console.error)
+);
 
 module.exports = {requestError, logOutError};
