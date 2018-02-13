14fe2b4d59959afe429accdabfe58f1e76f5c2bb
Braagaa <michael_braga@outlook.com>
2018-02-12 20:17:11 -0500

    Documentation is more less complete with working JsDoc

--- js/time.js
+++ js/time.js
@@ -1,12 +1,24 @@
 const moment = require('moment');
 
 /**
+ * momentjs wrapper module
+ * @module js/time
+ * @see module:js/time
+ */
+
+/**
  * Creates the current date in a string.
  *
+ * @alias module:js/time.now
  * @param {string} format - Format for date
- * @returns {() => string} Formatted date
+ * @returns {function(): string}
  * @see {@link https://momentjs.com/docs/#/displaying/format/}
  */
-const now = format => () => moment().format(format);
+const now = format => 
+    /**
+     * @func
+     * @returns {string} Formatted date
+     */
+    () => moment().format(format);
 
 module.exports = {now};
